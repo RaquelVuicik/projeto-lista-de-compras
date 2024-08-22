dd37c28ae737c06f4584ecb046bb3793440ba6b0
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-08-22 11:12:46 -0300

    atualizacao lista de compras

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const item = document.querySelector('#input-item');
 const botaoSalvarItem = document.querySelector('#adicionar-botao');
-
+const listaDeCompras = document.querySelector('#lista-de-compras');
 
 botaoSalvarItem.addEventListener('click', adicionarItem);
 
@@ -17,5 +17,29 @@ function adicionarItem(evento) {
     nomeDoItem.innerText = item.value;
     containerNomeDoItem.appendChild(nomeDoItem);
 
+    const containerBotoes = document.createElement('div');
+    const botaoRemover = document.createElement('button');
+    botaoRemover.classList.add('botao-acao');
+
+    const imagemRemover = document.createElement('img');
+    imagemRemover.src = 'img/delete.svg';
+    imagemRemover.alt = 'Remover';
+
+    botaoRemover.appendChild(imagemRemover);
+    containerBotoes.appendChild(botaoRemover);
+
+    const botaoEditar = document.createElement('button');
+    botaoEditar.classList.add('botao-acao');
+
+    const imagemEditar = document.createElement('img');
+    imagemEditar.src = 'img/edit.svg';
+    imagemEditar.alt = 'Editar';
+
+    botaoEditar.appendChild(imagemEditar);
+    containerBotoes.appendChild(botaoEditar);
+
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
     itemDaLista.appendChild(containerItemLista);
+    listaDeCompras.appendChild(itemDaLista);
 }
