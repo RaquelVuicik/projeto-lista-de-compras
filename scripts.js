29b4eaa00bd1bf2ef45695a2720aed9d80f3c064
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-08-22 10:44:13 -0300

    add item da lista

--- scripts.js
+++ scripts.js
@@ -6,5 +6,16 @@ botaoSalvarItem.addEventListener('click', adicionarItem);
 
 function adicionarItem(evento) {
     evento.preventDefault()
-    console.log('entrei na função');
+    // console.log('entrei na função');
+
+    const itemDaLista = document.createElement('li');
+    const containerItemLista = document.createElement('div');
+    containerItemLista.classList.add('item-lista-container');
+
+    const containerNomeDoItem = document.createElement('div');
+    const nomeDoItem = document.createElement('p');
+    nomeDoItem.innerText = item.value;
+    containerNomeDoItem.appendChild(nomeDoItem);
+
+    itemDaLista.appendChild(containerItemLista);
 }
